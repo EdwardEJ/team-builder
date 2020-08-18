08d9da2d28bafa99d884525d30638f25ac9ff5f3
A <jesus.e.estrada@hotmail.com>
2020-08-18 17:54:12 -0500

    started form, buggy

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const initialFormValue = [
+  {
+    id: 1,
+    username: 'ed',
+  }
+]
+
 function App() {
+  const [teamates, setTeamates] = useState(initialFormValue)
+
+  const [username, setUserName] = useState('')
+
+  const [userNameValue, setUserNameValue] = useState('') //used for storing event target
+
+
+  const onChange = e => {
+    setUserNameValue(e.target.value) //sets userNameValue with setUserNameValue setter function
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    setUserName(userNameValue) //sets username with setUserName setting function
+  }
+
+  // useEffect(() => {
+  //   setTeamates({ ...teamates, username })
+  // })
+
+  console.log(teamates)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          value={userNameValue}
+          type='text'
+          placeholder='username'
+          onChange={onChange}
+        />
+      </form>
+
+      {
+        teamates.map((teamate) =>
+          <li key={teamate.id}>{teamate}</li>
+        )
+      }
     </div>
   );
 }
